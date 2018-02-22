b199579c8566c76a228e660c95201ec470ef9e89
weblips <weblips@gmail.com>
2018-02-22 09:56:42 +0200

    start UA/RU Pack

---
+++ admin/language/ua-uk/common/footer.php
@@ -0,0 +1,5 @@
+<?php
+// Text
+$_['text_footer'] 	= '<HR><a href="https://www.opencart.com/">OpenCart</a> &copy; 2009-' . date('Y') . ' Всі права захищені.<br> <BR> <a href="https://alius.com.ua/" target="_blank">Український переклад OpenCart Аліус';
+$_['text_version'] 	= 'Version %s (trs-3)';
+
